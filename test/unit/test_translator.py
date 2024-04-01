7b7fdf3662877bae075cb852a6687a0ef49f6360
csuraparaju <csuraparaju@wpi.edu>
2024-04-01 18:38:49 -0400

    pass linter hopefully

--- test/unit/test_translator.py
+++ test/unit/test_translator.py
@@ -10,18 +10,15 @@ def test_correct_return_type(mocker):
 
 def test_lang_valid_input(mocker):
     mocker.patch("src.translator.translate_content", return_value=(False, "This is a Chinese message"))
-    is_english, translated_content = translate_content("这是一条中文消息")
-    assert is_english == False
-    assert translated_content == "This is a Chinese message"
+    res = translate_conten("这是一条中文消息")
+    assert res == (False, "This is a Chinese message")
 
     mocker.patch("src.translator.translate_content", return_value=(True, "This is an English message"))
-    is_english, translated_content = translate_content("这是一条英文消息")
-    assert is_english == True
-    assert translated_content == "This is an English message"
+    res = translate_content("这是一条英文消息")
+    assert res == (True, "This is an English message")
 
 def test_invalid_lang(mocker):
     mocker.patch("src.translator.translate_content", return_value=(None, "Failed translating this post"))
-    is_english, translate_content = translate_content("eregtfderfegf")
-    assert is_english == None
-    assert translate_content == "Failed translating this post"
+    res = translate_content("sdfgdsfadfd")
+    assert res == (None, "Failed translating this post")
 
